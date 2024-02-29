ee1ab156fd2d5c31282ddfc5c993e7f1a7403b09
Juyeong Jeong <juyeong.jeong@riiid.co>
2024-02-29 16:14:44 +0900

    Add has-button-blank attribute

--- src/components/prefixed-line.tsx
+++ src/components/prefixed-line.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect, useMemo, useRef, useState } from "react";
 import componentify from "../componentify";
 import { C } from "../react";
 import type { WAMLComponent } from "../types";
@@ -6,14 +6,27 @@ import useWAML from "../use-waml";
 import IsoprefixedLineGroupRenderer from "./isoprefixed-line-group-renderer";
 
 const PrefixedLine:WAMLComponent<'PrefixedLine'> = ({ node, type, depth, className, ...props }) => {
+  const $ = useRef<HTMLDivElement>(null);
   const { renderingVariables, commonOptions } = useWAML();
-  let pendingClassName = renderingVariables.pendingClasses.pop();
+  const [ hasButtonBlank, setHasButtonBlank ] = useState(false);
 
-  if(pendingClassName && commonOptions.prefixedLineClassMap?.[pendingClassName]){
-    pendingClassName = commonOptions.prefixedLineClassMap[pendingClassName];
-  }
+  const pendingClassName = useMemo(() => {
+    let R = renderingVariables.pendingClasses.pop();
+
+    if(R && commonOptions.prefixedLineClassMap?.[R]){
+      R = commonOptions.prefixedLineClassMap[R];
+    }
+    return R;
+  }, [ commonOptions.prefixedLineClassMap, renderingVariables.pendingClasses ]);
+
+  useLayoutEffect(() => {
+    if($.current?.querySelector(".ButtonBlank")){
+      setHasButtonBlank(true);
+    }
+  }, []);
+
   return (
-    <div className={C(className, type, pendingClassName)} {...props}>
+    <div ref={$} className={C(className, type, pendingClassName)} {...props} {...hasButtonBlank ? { 'data-has-button-blank': true } : {}}>
       <IsoprefixedLineGroupRenderer depth={depth} lines={node} />
     </div>
   );
